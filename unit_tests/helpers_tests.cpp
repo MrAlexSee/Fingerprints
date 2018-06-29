b11e48dbdc33fa3e3771a03402fe1e2ddd993b5a
Aleksander Cisłak <alex.cis91@gmail.com>
2018-06-29 11:19:51 +0200

    Added unit test for total size calculation with randomized strings.

--- unit_tests/helpers_tests.cpp
+++ unit_tests/helpers_tests.cpp
@@ -8,10 +8,39 @@ using namespace std;
 namespace fingerprints
 {
 
+namespace
+{
+
+constexpr int maxNStrings = 100;
+constexpr int stringSize = 50;
+
+}
+
 TEST_CASE("is getting total size for empty vector correct", "[files]")
 {
     vector<string> vec;
     REQUIRE(Helpers::getTotalSize(vec) == 0);
+}
+
+TEST_CASE("is getting total size correct", "[files]")
+{
+    vector<string> vec { "ala", "ma", "kota" };
+    REQUIRE(Helpers::getTotalSize(vec) == 9);
+}
+
+TEST_CASE("is getting total size for randomized strings correct", "[files]")
+{
+    for (int nStrings = 0; nStrings < maxNStrings; ++nStrings)
+    {
+        vector<string> vec;
+        
+        for (int iStr = 0; iStr < nStrings; ++iStr)
+        {
+            vec.emplace_back(string(stringSize, 'a'));
+        }
+
+        REQUIRE(Helpers::getTotalSize(vec) == nStrings * stringSize);
+    }
 }
 
 TEST_CASE("is reading words correct", "[files]")
