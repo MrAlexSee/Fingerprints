4fd8eec64aae47af8a053464d7253fc9f7d0fa6b
Aleksander Cisłak <alex.cis91@gmail.com>
2018-08-18 23:07:35 +0200

    Updated script for testing real data.

--- test_real.sh
+++ test_real.sh
@@ -5,13 +5,20 @@ outFile="res"
 
 dist=0
 k=1
+nIter=1
 
-for fType in $(seq -1 3);
+# Words only (no fingerprints).
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_urls.txt ${inputDir}/queries_urls_69.txt;
+
+# All fingerprint and letters type combinations.
+for fType in $(seq 0 3);
 do
     for lType in $(seq 0 2);
     do
-            ./fingerprints -d -o ${outFile}.txt -f $fType -l $lType -k $k -D $dist ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
-            ./fingerprints -d -o ${outFile}.txt -f $fType -l $lType -k $k -D $dist ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
-            ./fingerprints -d -o ${outFile}.txt -f $fType -l $lType -k $k -D $dist ${inputDir}/dict_urls.txt ${inputDir}/queries_urls_69.txt;
+        ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f $fType -l $lType ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
+        ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f $fType -l $lType ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
+        ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f $fType -l $lType ${inputDir}/dict_urls.txt ${inputDir}/queries_urls_69.txt;
     done
 done
