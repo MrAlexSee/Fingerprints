1ca4808c5cdf79587d070a2a3637fde00f84e0c9
Aleksander Cisłak <alex.cis91@gmail.com>
2018-08-18 22:29:44 +0200

    Updated processing synthetic data.

--- test_synth.sh
+++ test_synth.sh
@@ -5,9 +5,16 @@ outFile="res"
 
 dist=0
 k=1
-nIter=100
+nIter=1
 
-for fType in $(seq -1 3);
+# Words only (no fingerprints).
+for patternSize in $(seq 2 34);
+do
+    ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 --pattern-size $patternSize ${inputDir}/dict_synth.txt ${inputDir}/queries_synth.txt;
+done
+
+# All fingerprint and letters type combinations.
+for fType in $(seq 0 3);
 do
     for lType in $(seq 0 2);
     do
