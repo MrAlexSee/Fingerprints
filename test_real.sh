e3d7a7a7aa3c32f38827244632132169a9f4d99b
Aleksander Cisłak <alex.cis91@gmail.com>
2018-08-26 22:43:57 +0200

    Fixed dictionary order in testing real script.

--- test_real.sh
+++ test_real.sh
@@ -8,8 +8,8 @@ k=1
 nIter=1
 
 # Words only (no fingerprints).
-./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
 ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
 ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_urls.txt ${inputDir}/queries_urls_69.txt;
 
 # All fingerprint and letters type combinations.
