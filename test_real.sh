fece075d99316fbaf1e4e2c9a2137b7c9c1b7aaa
Aleksander Cisłak <alex.cis91@gmail.com>
2018-09-09 15:57:10 +0200

    Updated testing scripts to use new cmd-line parameters.

--- test_real.sh
+++ test_real.sh
@@ -3,19 +3,19 @@
 inputDir="data"
 outFile="res"
 
-dist=0
+dist=ham
 k=1
 nIter=1
 
 # Words only (no fingerprints).
-./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
-./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
-./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 ${inputDir}/dict_urls.txt ${inputDir}/queries_urls_69.txt;
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f none ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f none ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
+./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f none ${inputDir}/dict_urls.txt ${inputDir}/queries_urls_69.txt;
 
 # All fingerprint and letters type combinations.
-for fType in $(seq 0 3);
+for fType in none occ occhalved count pos;
 do
-    for lType in $(seq 0 2);
+    for lType in common mixed rare;
     do
         ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f $fType -l $lType ${inputDir}/dict_iamerican_insane.txt ${inputDir}/queries_iamerican_insane_8.txt;
         ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f $fType -l $lType ${inputDir}/dict_english_200.txt ${inputDir}/queries_english_200_8.txt;
--- test_synth.sh
+++ test_synth.sh
@@ -3,20 +3,20 @@
 inputDir="data"
 outFile="res"
 
-dist=0
+dist=ham
 k=1
 nIter=1
 
 # Words only (no fingerprints).
 for patternSize in $(seq 2 34);
 do
-    ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f -1 --pattern-size $patternSize ${inputDir}/dict_synth.txt ${inputDir}/queries_synth.txt;
+    ./fingerprints -d -o ${outFile}.txt -D $dist -k $k --iter $nIter -f none --pattern-size $patternSize ${inputDir}/dict_synth.txt ${inputDir}/queries_synth.txt;
 done
 
 # All fingerprint and letters type combinations.
-for fType in $(seq 0 3);
+for fType in none occ occhalved count pos;
 do
-    for lType in $(seq 0 2);
+    for lType in common mixed rare;
     do
         for patternSize in $(seq 2 34);
         do
