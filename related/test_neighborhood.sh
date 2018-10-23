82f48f7f0718c7bb330044278757c4eff025e098
Aleksander Cisłak <alex.cis91@gmail.com>
2018-10-23 10:34:10 +0200

    Updated the script for neighborhood testing to include new dictionary paths.

--- related/test_neighborhood.sh
+++ related/test_neighborhood.sh
@@ -8,21 +8,21 @@ outFile="res"
 
 # The number of errors and iterations selected here apply only to the fingerprints executable.
 k=1
-nIter=1
+nIter=100
 # Dictionary subsamples of given sizes must have been generated previously.
-sampleSizes=(64 256 1024 4096 16384)
+sampleSizes=(64 128 256 512 1024 2048 4096 8192 16384 32768)
 
 # Both fingerprints and neighborhood executables should be located in the current directory.
 for sampleSize in ${sampleSizes[@]$};
 do
-    ./neighborhood ${inputDir}/dict_${dictName}_${sampleSize}.txt ${inputDir}/queries_${dictName}_8.txt $distanceType
+    ./neighborhood ${inputDir}/subsampled/dict_${dictName}_${sampleSize}.txt ${inputDir}/queries_${dictName}_8.txt $distanceType
 
     # All fingerprint and letters type combinations.
     for fType in occ occhalved count pos;
     do
         for lType in common mixed rare;
         do
-            ./fingerprints -d -o ${outFile}.txt -D $distanceType -k $k --iter $nIter -f $fType -l $lType ${inputDir}/dict_${dictName}_${sampleSize}.txt ${inputDir}/queries_${dictName}_8.txt;
+            ./fingerprints -d -o ${outFile}.txt -D $distanceType -k $k --iter $nIter -f $fType -l $lType ${inputDir}/subsampled/dict_${dictName}_${sampleSize}.txt ${inputDir}/queries_${dictName}_8.txt;
         done
     done
 done
